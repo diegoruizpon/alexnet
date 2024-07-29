c0aed7b7923af328fc347406eef4ebe62c8fa666
Diego RUIZ PONSODA <diego.ruiz-ponsoda@student-cs.fr>
2024-07-29 09:05:54 +0200

    yolo archi

---
+++ yolo/yolo.py
@@ -0,0 +1,149 @@
+import torch 
+import torch.nn as nn
+
+
+
+class yolo(nn.Module):
+    def __init__(self):
+        super(yolo, self).__init__()
+        
+        self.leakyRELU = nn.LeakyReLU(negative_slope=0.1)
+        
+        ## Convolutional layers
+        
+        self.conv1 = nn.Conv2d(in_channels=3,
+                               out_channels=64,
+                               kernel_size=7,
+                               stride = 2)
+        
+        self.conv2 = nn.Conv2d(in_channels=64,
+                               out_channels=192,
+                               kernel_size=(3,3),
+                               )
+        
+        # Conv 3
+        
+        self.conv3_1 = nn.Conv2d(in_channels=192,
+                               out_channels=128,
+                               kernel_size=(1,1),
+                               )
+                                      
+        self.conv3_2 = nn.Conv2d(in_channels=128,
+                               out_channels=256,
+                               kernel_size=(3,3),
+                               )
+        
+        self.conv3_3 = nn.Conv2d(in_channels=256,
+                               out_channels=256,
+                               kernel_size=(1,1),
+                               )
+        
+        self.conv3_4 = nn.Conv2d(in_channels=256,
+                               out_channels=512,
+                               kernel_size=(3,3),
+                               )
+        
+        
+        # Conv 4
+        
+        ## Conv 4.1 x4
+        self.conv4_1 = nn.Conv2d(in_channels=512,
+                               out_channels=256,
+                               kernel_size=(1,1),
+                               )
+                                      
+        self.conv4_2 = nn.Conv2d(in_channels=256,
+                               out_channels=512,
+                               kernel_size=(3,3),
+                               )
+        
+        ## Conv 4.2
+        
+        self.conv4_3 = nn.Conv2d(in_channels=512,
+                               out_channels=512,
+                               kernel_size=(1,1),
+                               )
+        
+        
+        self.conv4_4 = nn.Conv2d(in_channels=512,
+                               out_channels=1024,
+                               kernel_size=(3,3)
+                               )
+        
+        
+        # Conv 5
+        
+        # Conv 5.1 x2
+        
+        self.conv5_1 = nn.Conv2d(in_channels=1024,
+                               out_channels=512,
+                               kernel_size=(1,1),
+                               )
+        
+        self.conv5_2 = nn.Conv2d(in_channels=512,
+                               out_channels=1024,
+                               kernel_size=(3,3),
+                               )
+        
+        # conv 5.2
+        
+        
+        self.conv5_3 = nn.Conv2d(in_channels=1024,
+                               out_channels=1024,
+                               kernel_size=(3,3),
+                               )
+        
+        self.conv5_4 = nn.Conv2d(in_channels=512,
+                               out_channels=1024,
+                               kernel_size=(3,3),
+                               stride = (2,2)
+                               )
+        
+        # Conv 6 
+        
+        self.conv6_1 = nn.Conv2d(in_channels=1024,
+                               out_channels=1024,
+                               kernel_size=(3,3),
+                               )
+        self.conv6_2 = nn.Conv2d(in_channels=1024,
+                               out_channels=1024,
+                               kernel_size=(3,3),
+                               )
+        
+        
+        ## Linear layers
+        
+        self.linear1 = nn.Linear(in_features=1024, out_features=4096)
+        
+        
+        self.linear2 = nn.Linear(in_features=4096, out_features=30)
+        
+        self.maxpool = nn.MaxPool2d(kernel_size=2,
+                                    stride = 2)
+        
+        
+        
+    def forward(self, x): 
+        
+        x = self.maxpool(self.leakyRELU(self.conv1(x)))
+        
+        
+        return x
+        
+        
+
+
+sample = torch.randn(3, 454, 454)
+
+
+model = yolo()
+
+print(model(sample).shape)
+        
+        
+    
+        
+        
+        
+        
+        
